504306ab1251b813b535b5a0b80d9c0e65c93db3
SurekhaChandrarathna <surekhamaduhansi@gmail.com>
2021-04-23 01:57:20 +0530

    DonationsService class completed

--- GadgetBadget/src/com/DonationsService.java
+++ GadgetBadget/src/com/DonationsService.java
@@ -18,5 +18,45 @@ import org.jsoup.nodes.Document;
 
 @Path("/Donations")
 public class DonationsService {
+	
+
+	Donations DonationsObject = new Donations();
+	@GET
+	@Path("/")
+	@Produces(MediaType.TEXT_HTML)
+	public String readDonations()
+	 {
+		return DonationsObject.readDonations();
+	 }
+	
+	
+	
+	@POST
+	@Path("/InsertDonation")
+	@Consumes(MediaType.APPLICATION_FORM_URLENCODED)
+	@Produces(MediaType.TEXT_PLAIN)
+	public String insertDonations(@FormParam("name") String name,
+	 @FormParam("email") String email,
+	 @FormParam("amount") String amount,
+	 @FormParam("cardNumber") String cardNumber,
+	 @FormParam("CVC") String CVC)
+	{
+	 String output = DonationsObject.insertDonations(name, email, amount, cardNumber, CVC);
+	return output;
+	}
+
+	
+	
+	//Delete Projects Details :
+	@POST
+	@Path("/Delete")
+	@Consumes(MediaType.APPLICATION_FORM_URLENCODED)
+	@Produces(MediaType.TEXT_HTML)
+	public String Delete(@FormParam("donationID") String donationID) throws SQLException
+	{
+		String output = DonationsObject.deleteDonation(donationID);
+		return output;
+
+	}
 
 }
