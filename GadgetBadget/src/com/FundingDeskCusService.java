32b37913b8129ec02690b0099ccbab93afc5ecec
SurekhaChandrarathna <surekhamaduhansi@gmail.com>
2021-04-23 02:24:40 +0530

    FundingDeskCus and FundingDeskCusService ,read all projects

---
+++ GadgetBadget/src/com/FundingDeskCusService.java
@@ -0,0 +1,31 @@
+package com;
+
+
+import model.Donations;
+import model.FundingDeskCus;
+//For REST Service
+import javax.ws.rs.*;
+import javax.ws.rs.core.MediaType;
+
+//For JSON
+import com.google.gson.*;
+
+//For XML
+import org.jsoup.*;
+import org.jsoup.parser.*;
+import org.jsoup.nodes.Document;
+
+@Path("/FundingDeskCus")
+public class FundingDeskCusService {
+	
+FundingDeskCus FundingObject = new FundingDeskCus();
+	
+	@GET
+	@Path("/")
+	@Produces(MediaType.TEXT_HTML)
+	public String readDonations()
+	 {
+		return FundingObject.readProjects();
+	 }
+
+}
