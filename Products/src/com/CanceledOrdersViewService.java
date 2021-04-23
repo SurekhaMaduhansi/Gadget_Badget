d6e4ca19808847287084f53bf48232e3942e0067
Sandamali <lakshika.sthennakoon@gmail.com>
2021-04-23 18:26:12 +0530

    7th update -(View Canceled Orders)

---
+++ Products/src/com/CanceledOrdersViewService.java
@@ -0,0 +1,25 @@
+package com;
+
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+
+import model.UserMessage;
+
+@Path("/CanceledView")
+public class CanceledOrdersViewService {
+	
+
+		    UserMessage canObj = new UserMessage();
+		
+		  	 
+		    @GET
+			@Path("/")
+			@Produces(MediaType.TEXT_HTML)
+			
+			public String AdminReadCanceledOr()
+			{
+				return canObj.AdminReadCanceledOr();
+		    }
+}
