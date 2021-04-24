900a534588380dc6c141c6b4b5d5b47babc50286
Sandamali <lakshika.sthennakoon@gmail.com>
2021-04-24 23:47:40 +0530

    user page styles updates

--- Products/src/com/AdminCheckService.java
+++ Products/src/com/AdminCheckService.java
@@ -1,6 +1,9 @@
 package com;
 
+import javax.ws.rs.Consumes;
+import javax.ws.rs.FormParam;
 import javax.ws.rs.GET;
+import javax.ws.rs.POST;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
@@ -22,4 +25,18 @@ public class AdminCheckService {
 			{
 				return checkObj.CheckOr();
 		    }
+		    
+		    @POST
+			@Path("/addCancelStatus")
+			@Consumes(MediaType.APPLICATION_FORM_URLENCODED)
+			@Produces(MediaType.TEXT_PLAIN)
+			public String insertCancelStatus(
+			@FormParam("email") String email,
+			@FormParam("ID") int ID,
+			@FormParam("status") String status)
+
+			{
+			String output = checkObj.insertCancelStatus(email,ID,status);  
+			return output;
+			}
 }
