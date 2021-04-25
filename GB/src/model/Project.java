bf4e826cec058b2fc83f6e45218db27a9bb2a01d
shey9855 <shehaniwije1998@gmail.com>
2021-04-25 19:40:40 +0530

    modifying data insert page

--- GB/src/model/Project.java
+++ GB/src/model/Project.java
@@ -75,59 +75,167 @@ public class Project {
 			
 			output ="<html>\r\n" + 
 			 		"<head>\r\n" + 
-			 		"<link rel=\"stylesheet\" href=\"../css/homee-style.css\">\r\n" + 
-			 		"<link rel=\"stylesheet\" href=\"https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css\">\r\n" + 
-			 		"<script src='https://kit.fontawesome.com/a076d05399.js'></script>\r\n" + 
+			 		//"<link rel=\"stylesheet\">\r\n" + 
+			 		"<link rel=\"stylesheet\"\r\n"
+			 		+ "	href=\"https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css\"\r\n"
+			 		+ "	integrity=\"sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T\"\r\n"
+			 		+ "	crossorigin=\"anonymous\">" + 
+			 		
 			 		"</head>\r\n" + 
 			 		"    <!--Header-->\r\n" + 
 			 		"    <br>\r\n" + 
-			 		"    <div class=\"topnav sticky\">\r\n" + 
+			 		"    <nav class=\"navbar navbar-expand-md navbar-dark\" style=\"background-color:black\">\r\n" + 
 			 		"    \r\n" + 
 			 		"         \r\n" + 
-			 		"            <center></center>\r\n" + 
-			 		"            <li><a  href=\"../../../GB/ProjectService/project_list/AddProject\">Add New Product <i class='fas fa-plus-square'></i></a></li>\r\n" + 
-			 		"            <li><a href=\"../../../GB/ProjectService/project_list/load\">All Projects and Manage project details <i class='fab fa-elementor'></i></a>\r\n" + 
 			 		
+			 		"            <div><a  href=\"../../../GB/ProjectService/project_list/AddProject\"><h5>Add New Product</h5> <i class='navbar-brand'></i></a>\r\n" +
+			 		
+			 		"            <a href=\"../../../GB/ProjectService/project_list/load\">All Projects and Manage project details <i class='nav-link'></i></a>\r\n" + 
+			 		"</div>"+
 			 		"  \r\n" + 
-			 		"          </div>\r\n" + 
-			 		"                    \r\n" + 
-			 		"              <div class=\"topnav sticky\"  >\r\n" + 
-			 		"               <div class=\"search-container\">\r\n" + 
-			 		"                <form action=\"searchHome.jsp\" method=\"post\">\r\n" + 
-			 		"					    	<input type = \"text\" placeholder=\"Search Item\" name= \"search\">\r\n" + 
-			 		"					    	<button type = \"Submit\">Search</button>\r\n" + 
-			 		"					    \r\n" + 
-			 		"					    </form>\r\n" + 
-			 		"               \r\n" + 
-			 		"             </div>\r\n" + 
-			 		"            </div>\r\n" + 
+			 		"          </nav>\r\n" + 
+			 		"                    \r\n" +
 			 		"           <br>\r\n" + 
 			 		"\r\n" + 
 			 		"<body>\r\n" + 
 			 		"	\r\n" +  
 			 		"			    \r\n" + 
 			 		"\r\n" + 
-			 		"		<center><div class=\"div1\">\r\n" + 
+			 		"		<center><div class=\"container col-md-5\">\r\n" + 
 			 		"		<form action='../../../GB/ProjectService/project_list/AddProject'  method='post'><br><b>\r\n" + 
 			 		"			<div class=\"card\">\r\n" + 
-			 		" 				 <img src=\"dd.png\"  style=\"width:50%\">\r\n" + 
-			 		"  					<div class=\"container\">\r\n" + 
-			 		"  				</div>\r\n" + 
-			 		"			</div>\r\n" + 
-			 		"			<br><br>\r\n" +
+			 		
+			 		"  					<div class=\"card-body\">\r\n" + 
+			 		
+			 		"			<br>\r\n" +
+			 		"<caption><h2>Add New Project</h2></caption>\r\n"+
+			 		"<br>"+
 			 		"			<input type=\"hidden\" name=\"ID\">" +
-			 		"			Project Code<br> <input type =\"text\" name = \"ProjectCode\" placeholder = \"Enter the project code..\" required><br><br>\r\n" + 
-			 		"			Project Name<br> <input type =\"text\" name = \"ProjectName\" placeholder = \"Enter the project name..\" required><br><br>\r\n" + 
-			 		"			Project Category<br> <input type =\"text\" name = \"Category\" placeholder = \"Enter the project category..\" required><br><br>\r\n" + 
-			 		"			Description<br> <input type =\"text\" name = \"Description\" required><br><br>\r\n" + 
-			 		"			Budget<br> <input type =\"text\" name = \"Budget\" placeholder = \"Enter the budget value..\" required><br><br>"
+			 		"			<fieldset class = 'form-group'><label>Project Code</label><br> <input type =\"text\" name = \"ProjectCode\" class ='form-control' placeholder = \"Enter the project code..\" required></fieldset>\r\n" + 
+			 		"			<fieldset class = 'form-group'><label>Project Name</label><br> <input type =\"text\" name = \"ProjectName\" class ='form-control' placeholder = \"Enter the project name..\" required></fieldset>\r\n" + 
+			 		"			<fieldset class = 'form-group'><label>Project Category</label><br> <input type =\"text\" name = \"Category\" class ='form-control' placeholder = \"Enter the project category..\" required></fieldset>\r\n" + 
+			 		"			<fieldset class = 'form-group'><label>Description</label><br> <input type =\"text\" name = \"Description\" class ='form-control' placeholder = \"Enter a short description..\" required></fieldset>\r\n" + 
+			 		"			<fieldset class = 'form-group'><label>Budget</label><br> <input type =\"text\" name = \"Budget\"class ='form-control' placeholder = \"Enter the budget value..\" required></fieldset>"
 			 		+ "\r\n"
 			 					+ "<input type='hidden'  name='UserEmail' value='shehani@GB.com'>" + 
-			 		"			<button class=\"button1\"><span>Add Project </span></button>\r\n" + 
+			 		"			<button class='btn btn-success'><span>Add Project </span></button>\r\n" + 
 			 		"				    \r\n" + 
 			 		"		</form>\r\n" + 
+			 		"  				</div>\r\n" + 
+			 		"			</div>\r\n" + 
 			 		"		</div></center>\r\n" + 
 			 		"		\r\n" + 
+			 		
+			 		//footer
+			 		"	<footer class=\"page-footer font-small color-dark\" style=\"background-color:#1f3a93\">\r\n" +
+		 			"\r\n" + 
+		 		"  <div style=\"background-color: #59abe3;\">\r\n" + 
+		 		"    <div class=\"container\">\r\n" + 
+		 		"\r\n" + 
+		 		"      <!-- Grid row-->\r\n" + 
+		 		"	<div class=\"row py-4 d-flex align-items-center\">\r\n" +
+		 		"\r\n"+
+		 		 " <!-- Grid column -->\r\n" +
+		 		"        <div class=\"col-md-6 col-lg-5 text-center text-md-left mb-4 mb-md-0\">\r\n" + 
+		 		"          <h6 class=\"mb-0\" style=\"color:white\">Get connected with us on social networks!</h6>\r\n" + 
+		 		"        </div>\r\n" + 
+		 		"        <!-- Grid column -->\r\n" + 
+		 		"\r\n" + 
+		 		"        <!-- Grid column -->\r\n" +
+		 		"        <div class=\"col-md-6 col-lg-7 text-center text-md-right\">\r\n" + 
+		 		"\r\n" + 
+		 		"          <!-- Facebook -->\r\n" +
+		 		"          <a class=\"fa fa-facebook\"></a>\r\n" + 
+		 		"          <!-- Twitter -->\r\n" +
+		 		"          <a class=\"fa fa-twitter \"></a>\r\n" +
+		 		"          <!-- Google +-->\r\n" + 
+		 		"          <a class=\"fa fa-google-plus-g\"></a>\r\n" + 
+		 		"          <!--Linkedin -->\r\n" +
+		 		"          <a class=\"fa fa-linkedin\"></a>\r\n" + 
+		 		"          <!--Instagram-->\r\n" + 
+		 		"          <a class=\"fa fa-instagram \"> </a>\r\n" + 
+		 		"\r\n" + 
+		 		"        </div>\r\n" + 
+		 		"        <!-- Grid column -->\r\n" + 
+		 		"\r\n" + 
+		 		"      </div>\r\n" + 
+		 		"      <!-- Grid row-->\r\n" + 
+		 		"\r\n" + 
+		 		"    </div>\r\n" + 
+		 		"  </div>\r\n" + 
+		 		"\r\n" + 
+		 		"  <!-- Footer Links -->\r\n" + 
+		 		"  <div class=\"container text-center text-md-left mt-5\">\r\n" +
+		 		"\r\n" + 
+		 		"    <!-- Grid row -->\r\n" + 
+		 		"    <div class=\"row mt-3\">\r\n" + 
+		 		"\r\n" + 
+		 		"      <!-- Grid column -->\r\n" + 
+		 		"      <div class=\"col-md-3 col-lg-4 col-xl-3 mx-auto mb-4\">\r\n" + 
+		 		"\r\n" + 
+		 		"        <!-- Content -->\r\n" + 
+		 		"        <h6 class=\"text-uppercase font-weight-bold\" >GadgetBadget Company</h6>\r\n" + 
+		 		"        <hr class=\"deep-purple accent-2 mb-4 mt-0 d-inline-block mx-auto\" style=\"width: 60px;\">\r\n" + 
+		 		"        <p style=\"color:white\">Best funding company for young researchers....</p>\r\n" + 
+		 		"\r\n" +
+		 		"      </div>\r\n" + 
+		 		"      <!-- Grid column -->\r\n" + 
+		 		"\r\n" + 
+		 		"      <!-- Grid column -->\r\n" + 
+		 		"      <div class=\"col-md-2 col-lg-2 col-xl-2 mx-auto mb-4\">\r\n" + 
+		 		"\r\n" + 
+		 		"        <!-- Links -->\r\n" + 
+		 		"        <h6 class=\"text-uppercase font-weight-bold\">Products</h6>\r\n" + 
+		 		"        <hr class=\"deep-purple accent-2 mb-4 mt-0 d-inline-block mx-auto\" style=\"width: 60px;\">\r\n" + 
+		 		"        <p>\r\n" + 
+		 		"          <a href=\"#!\" style=\"color:white\">pen Drives</a>\r\n" + 
+		 		"        </p>\r\n" + 
+		 		"        <p>\r\n" + 
+		 		"          <a href=\"#!\" style=\"color:white\">Hard Disks</a>\r\n" + 
+		 		"        </p>\r\n" + 
+		 		"        <p>\r\n" + 
+		 		"          <a href=\"#!\" style=\"color:white\">T shirts</a>\r\n" + 
+		 		"        </p>\r\n" + 
+		 		"        <p>\r\n" + 
+		 		"          <a href=\"#!\" style=\"color:white\">Cables</a>\r\n" + 
+		 		"		</p>\r\n" + 
+		 		"\r\n" +
+		 		"      </div>\r\n" + 
+		 		"      <!-- Grid column -->\r\n" + 
+		 		"\r\n" +
+		 		"      <!-- Grid column -->\r\n" + 
+		 		"      <div class=\"col-md-4 col-lg-3 col-xl-3 mx-auto mb-md-0 mb-4\">\r\n" + 
+		 		"\r\n" + 
+		 		"        <!-- Links -->\r\n" + 
+		 		"        <h6 class=\"text-uppercase font-weight-bold\">Contact</h6>\r\n" +
+		 		"        <hr class=\"deep-purple accent-2 mb-4 mt-0 d-inline-block mx-auto\" style=\"width: 60px;\">\r\n" + 
+		 		"        <p style=\"color:white\">\r\n" +
+		 		"          <i class=\"fa fa-home mr-3\"></i> Colombo, SriLanka</p>\r\n" + 
+		 		"        <p style=\"color:white\">\r\n" + 
+		 		"          <i class=\"fa fa-envelope mr-3\" style=\"color:white\"></i> info@example.com</p>\r\n" +
+		 		"        <p style=\"color:white\">\r\n" + 
+		 		"          <i class=\"fa fa-phone mr-3\" style=\"color:white\"></i> + 01 234 567 88</p>\r\n" +
+		 		"        <p style=\"color:white\">\r\n" + 
+		 		"          <i class=\"fa fa-print mr-3\" style=\"color:white\"></i> + 01 234 567 89</p>\r\n" + 
+		 		"\r\n" + 
+		 		"      </div>\r\n" + 
+		 		"      <!-- Grid column -->\r\n" + 
+		 		"\r\n" +
+		 		"      </div>\r\n" + 
+		 		"      <!-- Grid column -->\r\n" +
+		 		"\r\n" +
+		 		"  </div>\r\n" + 
+		 		"  <!-- Footer Links -->\r\n" + 
+		 		"\r\n" +
+		 		"  <!-- Copyright -->\r\n" + 
+		 		"  <div class=\"footer-copyright text-center py-3\" style=\"color:white\">© 2020 Copyright:\r\n" + 
+		 		"    <a href=\"https://mdbootstrap.com/\" style=\"color:white\"> GadgetBadget Company</a>\r\n" + 
+		 		"  </div>\r\n" + 
+		 		"  <!-- Copyright -->\r\n" + 
+		 		"\r\n" + 
+		 		"</footer>\r\n" +
+		 		"<!-- Footer -->\r\n" +
+		 		"\r\n" +
 			 		"</body>\r\n" + 
 			 		"</html>"; 
 				
@@ -181,7 +289,7 @@ public class Project {
 						"  <nav  class=\"navbar fixed-top navbar-white bg-white\">\r\n" + 
 						"		<div class= \"container\">\r\n" + 
 						"			<a class=\"navbar-brand\" href=\"#\">\r\n" + 
-						"     			 <img src=\"../images/Capture.PNG\" width=\"220\" height=\"78\" float=\"left\">\r\n" + 
+						"  <img src=\"../images/Capture.PNG\" width=\"220\" height=\"78\" float=\"left\">\r\n" +     			 
 						"   			</a>\r\n" + 
 						"   			<br>\r\n" + 
 						"   			<div class=\"topnav\" id=\"myTopnav\">\r\n" + 
@@ -198,7 +306,7 @@ public class Project {
 						"			<button class=\"btn\"><i class=\"fa fa-shopping-cart\" style=\"font-size:24px\"></i></button>\r\n" + 
 						"      \r\n" + 
 						"			<div class=\"dropdown\">\r\n" + 
-						"			   <img src=\"../images/avatar.png\" class=\"img-fluid\" alt=\"avatar1\" width=\"50\" height=\"80\" >\r\n" + 
+						"<img src=\"../images/avatar.png\" class=\"img-fluid\" alt=\"avatar1\" width=\"50\" height=\"80\" >\r\n" + 
 						"			  <div class=\"dropdown-content\">\r\n" + 
 						"			    <a href=\"#\">Profile</a>\r\n" + 
 						"			    <a href=\"\">Log Out</a>\r\n" + 
