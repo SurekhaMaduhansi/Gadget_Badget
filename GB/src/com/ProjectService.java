fca1aac6c82ea4ac6cc02c83dec502c18b97971a
shey9855 <shehaniwije1998@gmail.com>
2021-04-25 14:53:35 +0530

    Tested with Postman

--- GB/src/com/ProjectService.java
+++ GB/src/com/ProjectService.java
@@ -77,13 +77,24 @@ public class ProjectService {
 	@Path("/Delete")
 	@Consumes(MediaType.APPLICATION_FORM_URLENCODED)
 	@Produces(MediaType.TEXT_HTML)
+	public String Delete(@FormParam("ProjectId") String ProjectId) throws SQLException
+	{
+		String output = project.deleteProject(ProjectId);
+		return output;
+
+	}
+	
+	@DELETE
+	@Path("/delete")
+	@Consumes(MediaType.APPLICATION_XML)
+	@Produces(MediaType.TEXT_PLAIN)
 	public String deleteItem(String itemData)
 	{
 	//Convert the input string to an XML document
 	 Document doc = Jsoup.parse(itemData, "", Parser.xmlParser());
 
 	//Read the value from the element <itemID>
-	 String ProjectId = doc.select("itemID").text();
+	 String ProjectId = doc.select("ProjectId").text();
 	 String output = project.deleteProject(ProjectId);
 	return output;
 	}
